118d77b45a97e0cecd30f76cec354ee7b3d67a32
SrAlbert0 <73313619+SrAlbert0@users.noreply.github.com>
2024-11-07 19:58:26 +0000

    Coments cleaning

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -4,17 +4,11 @@ const Navbar = () =>{
     return(
         <nav className="navbar navbar-expand-lg bg-dark bg-opacity-75">
             <div className="container-fluid">
-              {/* Marca alineada a la izquierda */}
               <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
-
-              {/* Botón para la navbar responsive */}
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
-
-              {/* Contenido de la barra de navegación */}
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                {/* Uso de ms-auto para alinear estos elementos a la derecha */}
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                   <li className="nav-item">
                     <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
